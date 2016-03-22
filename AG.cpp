115c55bff34335871e48fff50ab3dcb65dcc9657
julia <jcarduini@gmail.com>
2016-03-22 12:27:06 -0300

    funcao de aptidao em construçao

--- AG.cpp
+++ AG.cpp
@@ -1,32 +1,52 @@
-#define tamIndividuo 10
-#define tamPopulacao 100
-#define taxaGeracao 50
-#define taxaMutacao 5
-#define tour 3
+/** INSTRUÇÕES PARA COMPILAR E EXECUTAR AG.cpp:
+ * g++ -o AG AG.cpp
+ * ./AG
+**/
+
+#define tamIndividuo 10	// solução
+#define tamPopulacao 100	// quantidade de indivíduos
+#define taxaGeracao 50	// porcentagem de filhos gerados (em relação a população)
+#define taxaMutacao 5	// porcentagem de filhos mutantes (em relação aos filhos)
+#define tour 3	// quantidade de indivíduos selecionados no torneio
 
 #include<iostream> 	//std::cout
 #include<algorithm> 	//std::shuffle
 
-
+/** matriz de população 
+ * linhas: população inicial + seus filhos
+ * colunas: tamIndividuo + função de aptidão + função de aptidão acumulada
+ **/
 int populacao[tamPopulacao+(tamPopulacao*taxaGeracao/100)][tamIndividuo+2];
 
-void geraPopulacao(int pop[tamPopulacao+(tamPopulacao*taxaGeracao/100)][tamIndividuo+2]){
+
+void aptidao(){
+	int i;
+	for (i = 0; i < tamPopulacao; i++)
+		populacao[i][tamIndividuo + 1] = (populacao[i][0]+populacao[i][*10^3 + populacao[i][1]*10^2 + populacao[i][2]*10^1 + populacao[i][3]) 
+
+
+void imprimePopulacao(){
+	int i, j;
+	for (i = 0; i < tamPopulacao; i++){
+		for (j = 0; j < tamIndividuo; j++)
+		  	std::cout<<populacao[i][j];	
+		std::cout<<"\n";
+	}
+}
+
+void geraPopulacao(){
 	int vetor[10] = {0,1,2,3,4,5,6,7,8,9}, i = 0, j;
 	std::cout<<"Gerando população\n";
 	while (i < tamPopulacao){
-		for (j = 0; j < tamIndividuo; j++){
-			//gera um vetor com permutações de 0 a 9
-			std::random_shuffle(vetor, vetor+10);
-			for (int k = 0; k < 10; k++)
-			  std::cout<<vetor[k];
-			std::cout<<"\n";
-		}
+		std::random_shuffle(vetor, vetor+10); 	// mistura os elementos de vetor
+		for (j = 0; j < 10; j++)
+			populacao[i][j]=vetor[j];	// coloca vetor na populacao
 		i++;
 	}
 }
 
 main(){
   srand((unsigned)time(NULL));  
-  geraPopulacao(populacao);
-
+  geraPopulacao();
+	imprimePopulacao();
 }
