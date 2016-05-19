50704f7233fda6af36b876216b2a6e4680dddd2f
julia <jcarduini@gmail.com>
2016-05-19 15:58:54 -0300

    distorcer as entradas 1 e 0 fazer testes com entradas distorcidas

--- trabalho2/perceptron.cpp
+++ trabalho2/perceptron.cpp
@@ -1,4 +1,137 @@
 #include<iostream> //std::cout
+#include<vector>
+
+int distorcidos[20][6][5]= {{
+	{0,1,1,1,0},
+	{1,0,0,0,1},
+	{1,0,0,0,1},
+	{1,0,0,0,1},
+	{1,0,0,0,1},
+	{0,1,1,1,0}},	{
+		{0,1,1,1,0},
+		{1,0,0,0,1},
+		{1,0,0,0,1},
+		{1,0,0,0,1},
+		{1,0,0,0,1},
+		{0,1,1,1,0}},	{
+			{0,1,1,1,0},
+			{1,0,0,0,1},
+			{1,0,0,0,1},
+			{1,0,0,0,1},
+			{1,0,0,0,1},
+			{0,1,1,1,0}}, {
+				{0,1,1,1,0},
+				{1,0,0,0,1},
+				{1,0,0,0,1},
+				{1,0,0,0,1},
+				{1,0,0,0,1},
+				{0,1,1,1,0}}, {
+					{0,1,1,1,0},
+					{1,0,0,0,1},
+					{1,0,0,0,1},
+					{1,0,0,0,1},
+					{1,0,0,0,1},
+					{0,1,1,1,0}}, {
+	{0,1,1,1,0},
+	{1,0,0,0,1},
+	{1,0,0,0,1},
+	{1,0,0,0,1},
+	{1,0,0,0,1},
+	{0,1,1,1,0}}, {
+		{0,1,1,1,0},
+		{1,0,0,0,1},
+		{1,0,0,0,1},
+		{1,0,0,0,1},
+		{1,0,0,0,1},
+		{0,1,1,1,0}}, { 
+			{0,1,1,1,0},
+			{1,0,0,0,1},
+			{1,0,0,0,1},
+			{1,0,0,0,1},
+			{1,0,0,0,1},
+			{0,1,1,1,0}}, {
+				{0,1,1,1,0},
+				{1,0,0,0,1},
+				{1,0,0,0,1},
+				{1,0,0,0,1},
+				{1,0,0,0,1},
+				{0,1,1,1,0}}, {
+					{0,1,1,1,0},
+					{1,0,0,0,1},
+					{1,0,0,0,1},
+					{1,0,0,0,1},
+					{1,0,0,0,1},
+					{0,1,1,1,0}},{ 
+	{0,0,1,0,0},
+	{0,1,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{1,1,1,1,1}}, {
+			{0,0,1,0,0},
+			{0,1,1,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
+			{1,1,1,1,1}}, {
+		{0,0,1,0,0},
+		{0,1,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{1,1,1,1,1}}, {
+	{0,0,1,0,0},
+	{0,1,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{1,1,1,1,1}}, {	
+		{0,0,1,0,0},
+		{0,1,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{1,1,1,1,1}}, {
+	{0,0,1,0,0},
+	{0,1,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{1,1,1,1,1}}, {
+		{0,0,1,0,0},
+		{0,1,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{1,1,1,1,1}}, {
+	{0,0,1,0,0},
+	{0,1,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{1,1,1,1,1}}, {
+		{0,0,1,0,0},
+		{0,1,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{1,1,1,1,1}}, {
+	{0,0,1,0,0},
+	{0,1,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{1,1,1,1,1}}};
+
+
+		
+
+
+
+
+
+
+
 
 /* Parâmetros fixos para problema 1 */
 
@@ -8,17 +141,49 @@ int bias = 1;
 int linhas = 6;
 int colunas = 5;
 
-int p0[6][5] = {
-	{0,1,1,1,0},
+int padrao[6][6][5] ={{
+		{0,1,1,1,0},
 	{1,0,0,0,1},
 	{1,0,0,0,1},
 	{1,0,0,0,1},
 	{1,0,0,0,1},
 	{0,1,1,1,0}
-	};
-int ft0 = 0;
-
-int p1[6][5]; 
+	},{
+	{0,0,1,0,0},
+	{0,1,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{1,1,1,1,1}
+},{
+{0,1,1,1,0},
+	{0,1,0,1,0},
+	{0,0,0,1,0},
+	{0,0,1,0,0},
+	{0,1,0,0,0},
+	{1,1,1,1,1}
+	},{
+	{0,1,1,1,1},
+	{0,0,0,0,1},
+	{0,1,1,1,1},
+	{0,0,0,0,1},
+	{0,0,0,0,1},
+	{0,1,1,1,1}
+	},{
+	{1,0,0,1,0},
+	{1,0,0,1,0},
+	{1,0,0,1,0},
+	{1,1,1,1,0},
+	{0,0,0,1,0},
+	{0,0,0,1,0}
+	},{
+	{0,1,1,1,1},
+	{0,1,0,0,0},
+	{0,1,1,1,1},
+	{0,0,0,0,1},
+	{0,0,0,0,1},
+	{0,1,1,1,1}
+	}};
 
 
 /* Parâmetros escolhidos pelo usuário */
@@ -27,19 +192,20 @@ int pesos = 1; /* Pesos zerados */
 int w [31]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int taxa = 1;
 int exercicio = 1;
-
+int ft[2] = {0,1};
 
 /* Funções */
 
-void menu(){
-	std::cout<<"Executar qual exercício?\n";
-	std::cin>>exercicio;
-	std::cout<<"Taxa de aprendizagem: ";
-	std::cin>>taxa;
-	std::cout<<"Pesos iniciais: \n1- Zerados\n2- Aleatórios\nOpção: "; 
-	std::cin>>pesos;
+/* Imprime os pesos */
+void printPesos(){
+	int i;
+	std::cout<<"Pesos: ";
+	for (i = 0; i< 31; i++)
+		std::cout<<w[i]<<' ';
+	std::cout<<'\n';
 }
 
+/* Imprime um padrão */
 void printPadrao(int p[][5])
 {
 	int i;
@@ -56,24 +222,42 @@ void printPadrao(int p[][5])
 	}
 }
 
+void menu(){
+
+	std::cout<<"Executar qual exercício?\n";
+	std::cin>>exercicio;
+	std::cout<<"Taxa de aprendizagem: ";
+	std::cin>>taxa;
+	std::cout<<"Pesos iniciais: \n1- Zerados\n2- Aleatórios\nOpção: "; 
+	std::cin>>pesos;
+}
+
+/* Funcção soma */		
 int soma(int p[][5], int w[30]){
-	
+
 	int i;
 	int j;
 	int k = 0;
-	int retorno = 0;
+	int sum = 0;
 	
+/* Soma ponderada com entradas xi e pesos wi */
 	for (i = 0; i < linhas; i++)
 		for (j = 0; j < colunas; j++){
-			retorno += p[i][j]*w[k];
+			sum = p[i][j]*w[k];
 			k++;
 		}			
-	soma += w[k]; 				/* Último peso do BIAS */
-	std::cout<<"soma: "<<retorno;
-	return retorno;
+	
+	sum += w[k]; /* Último peso =>  BIAS */
+
+	/* Função de ativação DEGRAU */
+	if (sum > 0)
+		return 1;
+	else 
+		return 0;
 }
 
-void ajuste(int [][5]p, int erro){
+/* Faz ajuste dos pesos de acordo com o padrão que não bateu */
+void ajuste(int p[][5], int w[], int erro){
 
 	int i;
 	int j;
@@ -87,17 +271,30 @@ void ajuste(int [][5]p, int erro){
 		w[k] = w[k] + erro*taxa*bias;
 }
 
-void exercicio1(){
+/* Faz treinamento com entradas 0 e 1 */
+int treinamento(int epocas){
 
 	int erro;
+	bool ajusta = false;
+
 	do{
-		erro = ft0 - soma(p0, w);
-		if (erro != 0)
-			ajuste(p0, w, erro);
-	}while(erro > 0); 
+		erro = ft[0] - soma(padrao[0], w);
+		if (erro != 0){
+			ajusta = true;
+			ajuste(padrao[0], w, erro);
+		}
+		erro = ft[1] - soma(padrao[1], w);
+		if (erro != 0){
+			ajusta = true;
+			ajuste(padrao[1], w, erro); 
+		}
+		epocas++;
+	}while(erro != 0 && ajusta); 
+
+	return epocas;
 }
 
-void controle(){
+void controle(int epocas){
 /*
 	switch(pesos){
 		case 1:			// pesos zerados 
@@ -105,13 +302,22 @@ void controle(){
 		}*/
 	switch(exercicio){
 		case 1:
-			soma(p0, w);
+			int i;
+			std::cout<<"Epocas: "<<treinamento(epocas)<<'\n';
+			printPesos();
+			
 			break;
 		}	
 	}
 
 int main(){
-//	printPadrao(p0);
+
+	printPadrao(padrao[2]);
+/*	printPadrao(p3);
+	printPadrao(p4);
+	printPadrao(p5);
+*/
+	int epocas = 0;
 //	menu();
-	controle();
+	controle(epocas);
 }
